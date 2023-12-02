dc31976da90f3147fe1d60d369831acdb97ba404
man301 <hoangman123456789p@gmail.com>
2023-12-02 18:04:05 +0700

    2 time

---
+++ WebBanHaiSan/Migrations/202311260918297_Update12061.cs
@@ -0,0 +1,18 @@
+namespace WebBanHaiSan.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update12061 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.tb_Product", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.tb_Product", "Price", c => c.Decimal(precision: 18, scale: 2));
+        }
+    }
+}
